09e5cdb82a9f0adb92587dda6b43504a32b07117
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-10-22 02:00:24 +0530

    Context Type in React

---
+++ src/GeekyShow/ContextText/ContextType/CTypeGuest.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {MyContextType} from "./Context"
+export default class CTypeGuest extends Component {
+    static contextType= MyContextType; 
+  render() {
+
+    return (
+      <div>
+         <h2>Context Type - Guest</h2> 
+                    <h4>Name : {this.context.data.name}</h4>
+                    <h4>Value : {this.context.data.value}</h4>
+                    <button onClick={this.context.handleEvent}>Change Value by +1</button>
+                    <button onClick={this.context.handleEvent2}>Change Value by -1</button>
+      </div>
+    )
+  }
+}
---
+++ src/GeekyShow/ContextText/ContextType/CTypeUser.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import CTypeGuest from "./CTypeGuest"
+export default class CTypeUser extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Context Type - User</h2>
+        <CTypeGuest />
+      </div>
+    )
+  }
+}
