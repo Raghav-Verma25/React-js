027198fbfe3610ca952a881d43fa62ab1502135a
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2023-01-28 12:00:24 +0530

    Custom component using class

--- src/App2.js
+++ src/App2.js
@@ -1,10 +1,13 @@
 import React , {component} from "react";
 import FirstComponent from "./Component2/FirstComponent"
+import SecondComponent from "./Component2/SecondComponent";
 
 function App2(){
     return(
     <>
     <FirstComponent/>
+    <br />
+    <SecondComponent/>
     </>
     )
 }
--- src/Component2/SecondComponent.jsx
+++ src/Component2/SecondComponent.jsx
@@ -3,12 +3,18 @@ import React, { Component } from 'react'
 
 
 class SecondComponent extends React.Component{
- 
+    render()     // render method is used to call jsx
+        {
+            return(
+                <>
+                <h2>This is Second Component</h2>
+                <h3>Using React class</h3>
+                </>
+            )
+        }
 }
 
-
-
-
+export default SecondComponent;
 
 
 
